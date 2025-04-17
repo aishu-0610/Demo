3281f9a1c35da5c28f7e5df35a8a19fe9250a368
bhavanaa-v <24pt06@psgtech.ac.in>
2025-04-17 15:31:40 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,27 +21,23 @@ private:
     int courseCount;
 
 public:
-    CourseGraph() {
-        courseCount = 0;
-    }
+    CourseGraph() : courseCount(0) {}
 
     void addCourse(const char* code, const char* name, const char prereqCodes[][10], const char prereqNames[][100], int prereqCount) {
-        if (courseCount >= MAX_COURSES) {
-            cout << "Cannot add more courses. Limit reached." << endl;
-            return;
-        }
         strcpy(courseList[courseCount].code, code);
+        strcpy(courseList[courseCount].name, name);
         courseList[courseCount].prereqCount = prereqCount;
-        
-        for (int i = 0; i < prereqCount; i++) {
+
+        for (int i = 0; i < prereqCount; ++i) {
             strcpy(courseList[courseCount].prereqCodes[i], prereqCodes[i]);
             strcpy(courseList[courseCount].prereqNames[i], prereqNames[i]);
         }
+
         courseCount++;
     }
 
     int findCourseIndex(const char* code) {
-        for (int i = 0; i < courseCount; i++) {
+        for (int i = 0; i < courseCount; ++i) {
             if (strcmp(courseList[i].code, code) == 0) {
                 return i;
             }
@@ -50,15 +46,24 @@ public:
     }
 
     void printPrerequisites(const char* code, int level = 0) {
-        int index = findCourseIndex(code);
-        if (index == -1) {
-            cout << string(level * 2, ' ') << "Course not found: " << code << endl;
+        if (strcmp(code, "NA") == 0) {
             return;
         }
+
+        int index = findCourseIndex(code);
+        if (index == -1) {
+            cout << "Course not found: " << code << endl;
+            return;
+        }
+
         const Course& course = courseList[index];
         cout << string(level * 2, ' ') << "Course: " << course.code << " - " << name << endl;
-        
+
         for (int i = 0; i < course.prereqCount; i++) {
+            if (strcmp(course.prereqCodes[i], "NA") == 0) {
+                cout << string(level * 2, ' ') << "Prerequisite: " << course.prereqNames[i] << endl;
+                continue;
+            }
             cout << string(level * 2, ' ') << "Prerequisite: " << course.prereqCodes[i] << " - " << course.prereqNames[i] << endl;
             printPrerequisites(course.prereqCodes[i], level + 1);
         }
@@ -70,29 +75,33 @@ public:
             cerr << "Error opening file: " << filename << endl;
             return;
         }
+
         char line[512];
         while (file.getline(line, sizeof(line))) {
             char code[10], name[100], prereqCodes[256], prereqNames[256];
-            sscanf(line, "%[^|]|%[^|]|%[^|]|%[^\n]", code, name, prereqCodes, prereqNames);
-            
+            sscanf(line, "%[^|]|%[^|]|%[^|]|%[^
+]", code, name, prereqCodes, prereqNames);
+
             char parsedPrereqCodes[MAX_PREREQS][10];
             char parsedPrereqNames[MAX_PREREQS][100];
             int prereqCount = 0;
-            
+
             char* token = strtok(prereqCodes, ",");
             while (token != nullptr && prereqCount < MAX_PREREQS) {
                 strcpy(parsedPrereqCodes[prereqCount], token);
                 prereqCount++;
                 token = strtok(nullptr, ",");
             }
+
             token = strtok(prereqNames, ",");
-            
-            for (int i = 0; i < prereqCount && token != nullptr; i++) {
+            for (int i = 0; i < prereqCount && token != nullptr; ++i) {
                 strcpy(parsedPrereqNames[i], token);
                 token = strtok(nullptr, ",");
             }
+
             addCourse(code, name, parsedPrereqCodes, parsedPrereqNames, prereqCount);
         }
+
         file.close();
     }
 };
